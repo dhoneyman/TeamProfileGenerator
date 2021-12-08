aa5904c84a043476b02bc1edbe23cfabbcd52652
Dylan Honeyman <dylan@dylans-iMac.local>
2021-12-08 16:50:58 -0600

    generate html function working

--- generateHTML.js
+++ generateHTML.js
@@ -1,70 +1,106 @@
-function generateHTML(employees) {
+function employeeCards(employees){
+
+  let cardStr = '';
   employees.forEach((employee) => {
-    let role = employee.role;
-    if (role === "Engineer") {
-      return `
-    <div class='container-fluid'>
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="name">${employee.name}</h5>
-              <h6 class="postition">Position</h6>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="employee-id list-group-item">ID: ${employee.id}</li>
-              <li class="email list-group-item">Email: ${employee.email}</li>
-              <li class="github list-group-item">Github: ${employee.github}</li>
-            </ul>
-            <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
+  let role = employee.getRole();
+  console.log(employee.getRole())
+  if (role === "Engineer") { cardStr += `
+  <div class='container-fluid'>
+      <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="name">${employee.name}</h5>
+            <h6 class="postition">Position</h6>
           </div>
-      </div>`;
-    }
-    if (role === "Manager") {
-      return `
-    <div class='container-fluid'>
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="name">${employee.name}</h5>
-              <h6 class="postition">Position</h6>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="employee-id list-group-item">ID: ${employee.id}</li>
-              <li class="email list-group-item">Email: ${employee.email}</li>
-              <li class="github list-group-item">Github: ${employee.officeNumber}</li>
-            </ul>
-            <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
+          <ul class="list-group list-group-flush">
+            <li class="employee-id list-group-item">ID: ${employee.id}</li>
+            <li class="email list-group-item">Email: ${employee.email}</li>
+            <li class="github list-group-item">Github: <a href='${employee.github}'></li>
+          </ul>
+          <div class="card-body">
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
           </div>
-      </div>`;
-    }
-    if (role === "Intern") {
-      return `
-    <div class='container-fluid'>
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="name">${employee.name}</h5>
-              <h6 class="postition">Position</h6>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="employee-id list-group-item">ID: ${employee.id}</li>
-              <li class="email list-group-item">Email: ${employee.email}</li>
-              <li class="github list-group-item">Github: ${employee.school}</li>
-            </ul>
-            <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>
-      </div>`;
-    }
-  });
-  return `
-    `;
+        </div>
+    </div>`;
 }
+  if (role === "Manager") { cardStr += `
+  <div class='container-fluid'>
+      <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="name">${employee.name}</h5>
+            <h6 class="postition">Position</h6>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="employee-id list-group-item">ID: ${employee.id}</li>
+            <li class="email list-group-item">Email: ${employee.email}</li>
+            <li class="office list-group-item">Office Number: ${employee.officeNumber}</li>
+          </ul>
+          <div class="card-body">
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+    </div>`;
+  }
+  if (role === "Intern") { cardStr += `
+  <div class='container-fluid'>
+      <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="name">${employee.name}</h5>
+            <h6 class="postition">Position</h6>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="employee-id list-group-item">ID: ${employee.id}</li>
+            <li class="email list-group-item">Email: ${employee.email}</li>
+            <li class="school list-group-item">School: ${employee.school}</li>
+          </ul>
+          <div class="card-body">
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+    </div>`;}
+
+  })
+  return cardStr;
+}
+
+
+function generateHTML(employees) {
+    return (`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> -->
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>TeamMemberGenerator</title>
+    
+    </head>
+      <body>
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+              <span class="navbar-brand mb-0 h1">TEAM DREAMTEAM</span>
+            </div>
+          </nav>
+          <div class='parent-div'>
+      ${employeeCards(employees)}
+
+          </div>
+              <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+              <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+              <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script></html>
+              <script src='./index.js'></script>
+      </body>
+    </html>`
+
+    )}
+
+
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,85 +1,92 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateHTML = require("./generateHTML");
-const Engineer = require('./lib/engineer');
-const Intern = require('./lib/intern');
-const Manager = require('./lib/manager');
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
 const employees = [];
 
+generateEmployees();
 
-const questions = inquirer
-  .prompt([
-    {
-      type: "list",
-      message: "Please select employee position.",
-      options: ["Engineer", "Intern", "Manager"],
-      name: "position",
-    },
-    {
-      type: "input",
-      message: "Please enter employees name.",
-      name: "name",
-    },
-    {
-      type: "input",
-      message: "Please enter employees id number.",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "Please enter employees email address.",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "Please enter employees GitHub URL.",
-      name: "github",
-      when: (data) => data.position === "Engineer",
-    },
-    {
-      type: "input",
-      message: "Please enter employees office number",
-      name: "officeNumber",
-      when: (data) => data.position === "Manager",
-    },
-    {
-      type: "input",
-      message: "Please enter school attended by intern",
-      name: "school",
-      when: (data) => data.position === "Intern",
-    },
-    {
-      type: "confirm",
-      message: "Would you like to enter another employee?",
-      default: false,
-      name: "addEmployee",
-    },
-  ])
-  .then((data) => {
-    let newEmployee;
-    if(data.position === 'Engineer'){
-      newEmployee = new Engineer(data.name, data.email, data.id, data.github);
-    }
-    if(data.position === 'Intern'){
-      newEmployee = new Intern(data.name, data.email, data.id, data.school);
-    }
-    if(data.position === 'Manager'){
-      newEmployee = new Manager(data.name, data.email, data.id, data.officeNumber);
-    }
+function generateEmployees() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Please select employee position.",
+        choices: ["Engineer", "Intern", "Manager"],
+        name: "position",
+      },
+      {
+        type: "input",
+        message: "Please enter employees name.",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Please enter employees id number.",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Please enter employees email address.",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Please enter employees GitHub URL.",
+        name: "github",
+        when: (data) => data.position === "Engineer",
+      },
+      {
+        type: "input",
+        message: "Please enter employees office number",
+        name: "officeNumber",
+        when: (data) => data.position === "Manager",
+      },
+      {
+        type: "input",
+        message: "Please enter school attended by intern",
+        name: "school",
+        when: (data) => data.position === "Intern",
+      },
+      {
+        type: "confirm",
+        message: "Would you like to enter another employee?",
+        default: false,
+        name: "addEmployee",
+      },
+    ])
+    .then((data) => {
+      let newEmployee;
+      if (data.position === "Engineer") {
+        newEmployee = new Engineer(data.name, data.email, data.id, data.github);
+      }
+      if (data.position === "Intern") {
+        newEmployee = new Intern(data.name, data.email, data.id, data.school);
+      }
+      if (data.position === "Manager") {
+        newEmployee = new Manager(
+          data.name,
+          data.email,
+          data.id,
+          data.officeNumber
+        );
+      }
 
-    employees.push(newEmployee);
-    if(data.addEmployee === true){
-      inquirer.prompt;
-    } else {
-      generateHTML(employees);
-    }
+      employees.push(newEmployee);
+      console.log(employees);
+      console.log(employees.length);
+      
 
-
-    
-    writeToFile(data);
-  });
-
-function writeToFile(data) {
+      if (data.addEmployee === true) {
+        generateEmployees();
+      } else {
+        writeToFile(employees);
+      }
+    });
+}
+function writeToFile(employees) {
   fs.writeFile("employees.html", generateHTML(employees), (err) =>
     err ? console.error(err) : console.log("Success!")
   );
